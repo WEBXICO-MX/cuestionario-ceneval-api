304d75702894bd655adee3fa69e2359ce83b0b2b
R. Eder Weiss <eder.weiss87@gmail.com>
2022-03-23 15:02:31 -0600

    Versión inicial

---
+++ routes/cuestionario.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+/* GET Cuestionario Ceneval. */
+router.get('/', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+    try{
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("saice");
+            dbo.collection("cuestionarios_ceneval").findOne({activo:false}, function(err, result) {
+            if (err) throw err;
+               res.send(result);
+              db.close();
+            });
+          });
+    } catch (error) {
+        res.status(500).send(`Error al obtener los datos: ${error}`)
+    }
+
+
+});
+
+module.exports = router;
